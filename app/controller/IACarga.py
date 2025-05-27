cc7459377949736f332e941dd589fff6c825cec7
victor ballesteros <victor.ballesteros@majorel.com>
2025-05-27 09:15:31 +0200

    nueva carga ia ruta global

--- app/controller/IACarga.py
+++ app/controller/IACarga.py
@@ -2,6 +2,29 @@ import subprocess
 import requests
 from app.service.Gestion_historia import GestionHistoria
 from app.utilities.Textos import instrucciones
+import winreg
+
+
+def encontrar_ruta_lm_studio():
+    try:
+        clave_registro = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+        for raiz in [winreg.HKEY_LOCAL_MACHINE, winreg.HKEY_CURRENT_USER]:
+            with winreg.OpenKey(raiz, clave_registro) as clave_desinstalar:
+                cantidad_subclaves = winreg.QueryInfoKey(clave_desinstalar)[0]
+                for i in range(cantidad_subclaves):
+                    try:
+                        nombre_subclave = winreg.EnumKey(clave_desinstalar, i)
+                        with winreg.OpenKey(clave_desinstalar, nombre_subclave) as subclave:
+                            nombre_mostrado = winreg.QueryValueEx(subclave, "DisplayName")[0]
+                            if "LM Studio" in nombre_mostrado:
+                                ubicacion_instalacion = winreg.QueryValueEx(subclave, "InstallLocation")[0]
+                                return ubicacion_instalacion
+                    except FileNotFoundError:
+                        continue
+    except Exception as error:
+        print("Error al buscar en el registro:", error)
+    return None
+
 
 class IA:
     def __init__(self):
@@ -11,7 +34,9 @@ class IA:
 
     def cargar_modelo(self):
         try:
-            directorio_lm_studio = r"C:\Users\jotxilla\AppData\Local\Programs\LM Studio"
+           # directorio_lm_studio = r"C:\Users\jotxilla\AppData\Local\Programs\LM Studio"
+            directorio_lm_studio = encontrar_ruta_lm_studio()
+            print(f"{directorio_lm_studio} rtuta")
             # Desactivar el modelo actual
             self.hermes_process = subprocess.Popen(
                 ['lms', 'unload', 'hermes-3-llama-3.2-3b'],
